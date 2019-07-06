6a7523ad86b898c9ed019e36d4f30f81b7ebf26f
luciorq <luciorqueiroz@gmail.com>
2019-07-06 11:29:11 -0300

    - added trimmomatic image

--- tools/build_images.sh
+++ tools/build_images.sh
@@ -1,36 +1,48 @@
+# Funcion definition
+build_docker_images() {
+  echo "First argument is as $1"
+  echo "Second argument is as $2"
+
+  IMAGE_NAME="$1"
+  IMAGE_VERSION="$2"
+
+  echo "### Building image $IMAGE_NAME v. $IMAGE_VERSION ###"
+  docker build --build-arg BUILD_VERSION=${IMAGE_VERSION} \
+    -t ${IMAGE_NAME}:${IMAGE_VERSION} dockerfiles/$(basename $IMAGE_NAME)
+  docker tag ${IMAGE_NAME}:${IMAGE_VERSION} ${IMAGE_NAME}:latest
+  docker push ${IMAGE_NAME}
+}
 
 cd ~/projects/docker_images
 
 docker login --username luciorq
 
+
 # For SRA tools
 # docker run --rm -u $UID luciorq/sra-tools fasterq-dump --help
-IMAGE_NAME=luciorq/sra-tools
-IMAGE_VERSION=2.9.6
-docker build --build-arg BUILD_VERSION=${IMAGE_VERSION} -t ${IMAGE_NAME}:${IMAGE_VERSION} dockerfiles/sra-tools
-docker tag ${IMAGE_NAME}:${IMAGE_VERSION} ${IMAGE_NAME}:latest
-docker push ${IMAGE_NAME}
+build_docker_images luciorq/sra-tools 2.9.6
+
 
 # For STAR aligner
 # docker run --rm -u $UID luciorq/star -h
-IMAGE_NAME=luciorq/star
-IMAGE_VERSION=2.7.1a
-docker build --build-arg BUILD_VERSION=${IMAGE_VERSION} -t ${IMAGE_NAME}:${IMAGE_VERSION} dockerfiles/star
-docker tag ${IMAGE_NAME}:${IMAGE_VERSION} ${IMAGE_NAME}:latest
-docker push ${IMAGE_NAME}
+build_docker_images luciorq/star 2.7.1a
 
 # For SAMtools
 # docker run --rm -u $UID luciorq/samtools samtools view --help
-IMAGE_NAME=luciorq/samtools
-IMAGE_VERSION=1.9
-docker build --build-arg BUILD_VERSION=${IMAGE_VERSION} -t ${IMAGE_NAME}:${IMAGE_VERSION} dockerfiles/samtools
-docker tag ${IMAGE_NAME}:${IMAGE_VERSION} ${IMAGE_NAME}:latest
-docker push ${IMAGE_NAME}
+build_docker_images luciorq/samtools 1.9
 
 # For Entrez-Direct
 # docker run --rm -u $UID luciorq/entrez-direct /bin/bash -c "esearch -db gene -query "VAMP" | efetch -format xml"
-IMAGE_NAME=luciorq/entrez-direct
-IMAGE_VERSION=11.6
-docker build --build-arg BUILD_VERSION=${IMAGE_VERSION} -t ${IMAGE_NAME}:${IMAGE_VERSION} dockerfiles/entrez-direct
-docker tag ${IMAGE_NAME}:${IMAGE_VERSION} ${IMAGE_NAME}:latest
-docker push ${IMAGE_NAME}
+build_docker_images luciorq/entrez-direct 11.6
+
+# For Trimmomatic
+# docker run --rm -u $UID luciorq/trimmomatic -version
+build_docker_images luciorq/trimmomatic latest
+
+# For Trimmomatic
+# docker run --rm -u $UID luciorq/trimmomatic -h
+#build_docker_images luciorq/bowtie2 XXXXX-x
+
+
+
+
--- tools/check_images.sh
+++ tools/check_images.sh
@@ -7,3 +7,4 @@ docker inspect --type=image luciorq/samtools:latest
 docker inspect --type=image luciorq/entrez-direct:latest
 
 #docker inspect "${image_id}:${image_tag}" > /dev/null 2>&1 || echo "does not exist!"
+
