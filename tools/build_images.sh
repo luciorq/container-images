dc08c56f1f8209062d9c1bcfb8b9668b7c1818db
luciorq <luciorqueiroz@gmail.com>
2020-08-21 19:51:10 -0300

    generalized platform-agnostic function name

--- tools/build_images.sh
+++ tools/build_images.sh
@@ -5,7 +5,7 @@ mkdir -p $SINGULARITY_REPO
 #  docker://luciorq/$(basename $IMAGE_NAME):${IMAGE_VERSION}
 
 # Funcion definition
-build_docker_images() {
+build_images() {
   echo "First argument is as $1"
   echo "Second argument is as $2"
 
@@ -41,47 +41,47 @@ docker login --username luciorq
 # For SRA tools
 # docker run --rm -u $UID luciorq/sra-tools fasterq-dump --help
 
-#build_docker_images luciorq/sra-tools 2.10.7
-build_docker_images luciorq/sra-tools latest
+#build_images luciorq/sra-tools 2.10.7
+build_images luciorq/sra-tools latest
 
 # For STAR aligner
 # docker run --rm -u $UID luciorq/star -h
-build_docker_images luciorq/star 2.7.3a
+build_images luciorq/star 2.7.3a
 
 # For SAMtools
 # docker run --rm -u $UID luciorq/samtools samtools view --help
 # docker run --rm -u $UID luciorq/samtools bcftools --help
-build_docker_images luciorq/samtools 1.10
+build_images luciorq/samtools 1.10
 
 # For Entrez-Direct
 # docker run --rm -u $UID luciorq/entrez-direct /bin/bash -c "esearch -db gene -query "VAMP" | efetch -format xml"
-build_docker_images luciorq/entrez-direct 13.8
+build_images luciorq/entrez-direct 13.8
 
 # For Trimmomatic
 # docker run --rm -u $UID luciorq/trimmomatic -version
-build_docker_images luciorq/trimmomatic latest
+build_images luciorq/trimmomatic latest
 
 # For Bowtie2
 # docker run --rm -u $UID luciorq/bowtie2 -h
-build_docker_images luciorq/bowtie2 2.3.5.1
+build_images luciorq/bowtie2 2.3.5.1
 
 # For rMATS with turbo
 # docker run --rm -u $UID --entrypoint run_rmats luciorq/rmats:4.1.0 --version
 # docker run --rm -u $UID luciorq/rmats run_rmats -h
-build_docker_images luciorq/rmats 4.1.0
+build_images luciorq/rmats 4.1.0
 
 # For deeptools
 # docker run --rm -u $UID luciorq/deeptools bamCoverage -h
 # docker run --rm -u $UID luciorq/deeptools bamCoverage --version
-build_docker_images luciorq/deeptools 3.4.3
+build_images luciorq/deeptools 3.4.3
 
 # For minimap2
 # docker run --rm -u $UID luciorq/minimap2 minimap2 -h
 # docker run --rm -u $UID luciorq/minimap2 --version
-build_docker_images luciorq/minimap2 2.17
+build_images luciorq/minimap2 2.17
 
 # For fastqc
 # docker run --rm -u $UID luciorq/fastqc --version
 # singularity exec /data/singularity_images/fastqc_0.11.9.sif fastqc -v
 # build_test luciorq/fastqc 0.11.9
-build_docker_images luciorq/fastqc 0.11.9
+build_images luciorq/fastqc 0.11.9
